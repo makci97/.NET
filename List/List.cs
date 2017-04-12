62840fbe4eb02fc977c636b66c309d6e94c7b687
makci.97@mail.ru <makci.97@mail.ru>
2017-04-12 23:37:54 +0300

    Add List

---
+++ List/List.cs
@@ -0,0 +1,137 @@
+﻿namespace List
+{
+    public class List<T>
+    {
+        private class Node
+        {
+            public Node next;
+            public Node prev;
+            public T data;
+        }
+
+        private Node head = null;
+        private Node tail = null;
+        private int size = 0;
+
+        public void PushBack(T t)
+        {
+            Node newNode = new Node();
+            newNode.next = tail;
+            newNode.prev = null;
+            newNode.data = t;
+
+            if (tail != null)
+            {
+                // List is not empty
+                tail.prev = newNode;
+            }
+            else
+            {
+                // List is empty, therefore head == null too
+                head = newNode;
+            }
+            tail = newNode;
+            ++size;
+        }
+
+        public void PushFront(T t)
+        {
+            Node newNode = new Node();
+            newNode.next = null;
+            newNode.prev = head;
+            newNode.data = t;
+
+            if (head != null)
+            {
+                // List is not empty
+                head.next = newNode;
+            }
+            else
+            {
+                // List is empty, therefore tail == null too
+                tail = newNode;
+            }
+            head = newNode;
+            ++size;
+        }
+
+        public T PopBack()
+        {
+            if (tail != null)
+            {
+                T temp = tail.data;
+                if (head == tail)
+                {
+                    // pop last elem
+                    head = tail = null;
+                }
+                else
+                {
+                    tail = tail.next;
+                }
+                --size;
+                return temp;
+            }
+            else
+            {
+                // List is empty
+                throw new System.InvalidOperationException("List is empty!");
+            }
+        }
+
+        public T PopFront()
+        {
+            if (head != null)
+            {
+                T temp = head.data;
+                if (head == tail)
+                {
+                    // pop last elem
+                    head = tail = null;
+                }
+                else
+                {
+                    head = head.prev;
+                }
+                --size;
+                return temp;
+            }
+            else
+            {
+                // List is empty
+                throw new System.InvalidOperationException("List is empty!");
+            }
+        }
+
+        public T Back()
+        {
+            if (tail != null)
+            {
+                return tail.data;
+            }
+            else
+            {
+                // List is empty
+                throw new System.InvalidOperationException("List is empty!");
+            }
+        }
+
+        public T Front()
+        {
+            if (head != null)
+            {
+                return head.data;
+            }
+            else
+            {
+                // List is empty
+                throw new System.InvalidOperationException("List is empty!");
+            }
+        }
+
+        public int Size()
+        {
+            return size;
+        }
+    }
+}
---
+++ List/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace List
+{
+    internal class Program
+    {
+        public static class Test<T>
+        {
+            public static void PushIntElems(List<int> list)
+            {
+                // create list: front{-1, 0, 1, 2}back
+                System.Console.WriteLine("Push {0} elems", 4);
+                list.PushBack(1);
+                list.PushFront(0);
+                list.PushBack(2);
+                list.PushFront(-1);
+            }
+
+            public static void PopElems(List<T> list)
+            {
+                System.Console.WriteLine("Front = {0}", list.Front());
+                System.Console.WriteLine("Back = {0}", list.Back());
+                System.Console.WriteLine("PopBack returned = {0}", list.PopBack());
+                System.Console.WriteLine("PopFront returned = {0}", list.PopFront());
+                System.Console.WriteLine("PopFront returned = {0}", list.PopFront());
+                System.Console.WriteLine("PopBack returned = {0}", list.PopBack());
+                System.Console.WriteLine("PopBack returned = {0}", list.PopBack());
+                System.Console.WriteLine("PopFront returned = {0}", list.PopFront());
+            }
+
+            public static void Size(List<T> list)
+            {
+                System.Console.WriteLine("Size = {0}", list.Size());
+            }
+
+        }
+
+        public static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+
+            for (int i = 0; i < 2; i++)
+            {
+                System.Console.WriteLine("{0} testing sprint:", i + 1);
+
+
+                Test<int>.Size(list);
+                Test<int>.PushIntElems(list);
+                Test<int>.Size(list);
+                try
+                {
+                    Test<int>.PopElems(list);
+                }
+                catch (System.Exception e)
+                {
+                    System.Console.WriteLine(e);
+                }
+                finally
+                {
+                    System.Console.WriteLine("Some strange thing");
+                }
+                Test<int>.Size(list);
+            }
+        }
+    }
+}
